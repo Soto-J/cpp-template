f442e74a4a3955da210d2373aeb5584672917c7e
John Soto <soto.ja415@gmail.com>
2024-10-23 22:14:34 -0400

    First Commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <fmt/core.h>
+
+int main(int, char**) {
+  std::cout << "Hello, from cmakeExample!\n";
+  fmt::print("Hello World!\n");
+}
---
+++ out/build/default/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/sotoj/Documents/Cpp/projects/example2
+# Build directory: C:/Users/sotoj/Documents/Cpp/projects/example2/out/build/default
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
